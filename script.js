0412bcfbe1b33b86dbb198ca5870d5b235bb05aa
Hadi Azeem <hadiazeem.azeemi@gmail.com>
2022-10-02 13:58:44 +0500

    14th exercise done

--- script.js
+++ script.js
@@ -105,3 +105,10 @@ friends.map(x => console.log(`Hi ${x}, Have a great time here.`));
 var Transport = ['Car', 'Jet', 'Motor Bike', 'Bicycle'];
 
 Transport.map(x => console.log(`My Favourite type of Transport is ${x}.`));
+
+// Exercise 14
+// Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+
+var guests = ['Muhammad Rizwan', 'Malik Farhat', 'Suleman Rehan', 'Muhammad Yahya'];
+
+guests.map(x => console.log(`Hey ${x}, You are invited to dinner at our house today.`));
